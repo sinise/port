77f0c33c9ee32833c5bd3d1b0b67547d3d2e9197
seastian <sebastian@momentos.dk>
2015-11-10 14:10:13 +0100

    sd

--- port/app.js
+++ port/app.js
@@ -9,6 +9,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 //var db = require('./model/db');
+var db = require('mongodb').MongoClient;
+var assert = require('assert');
+
 var app = express();
 
 // connect to mongo
