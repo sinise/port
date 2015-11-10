ece04366c48f7f1184aebbdd80f7b2217c0d4813
seastian <sebastian@momentos.dk>
2015-11-10 14:26:18 +0100

    sdf

--- port/app.js
+++ port/app.js
@@ -37,9 +37,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(express.static(__dirname + 'public'));
 app.use('/', routes);
 app.use('/users', users);
-app.use(express.static(__dirname + 'public'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
