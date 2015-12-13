7af8b1bb4064754617da3fd5fabf981307bd294c
seastian <sebastian@momentos.dk>
2015-12-13 16:28:11 +0100

    updated run system

--- runSystem.sh
+++ runSystem.sh
@@ -3,9 +3,9 @@
 
 docker stop $(sudo docker ps -a -q)
 docker rm $(sudo docker ps -a -q)
-#rm -rf port/node_modules
-#docker build -t seeeb/port .
+rm -rf port/node_modules
+docker build -t seeeb/port .
 docker pull mongo
 docker run --name thisMongo -v ~/port/mongoVol:/data/db -it -d mongo:latest
-docker run --rm --name nodejs --link thisMongo:db -p 80:80 -it -v ~/port/port:/www seeeb/port
+docker run -d --name nodejs --link thisMongo:db -p 80:80 -it -v ~/port/port:/www seeeb/port
 
