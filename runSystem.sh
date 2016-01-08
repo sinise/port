05366da05262371d8447f6d08bf4086bf37749d1
sebastian@lapela.dk <sebastian@lapela.dk>
2016-01-08 15:20:06 +0100

    demonised nodejs

--- runSystem.sh
+++ runSystem.sh
@@ -5,7 +5,7 @@
 cd ~/port
 #docker rm $(sudo docker ps -a -q)
 #rm -rf port/node_modules
-#docker build -t seeeb/port .
+#docker bui-d ld -t seeeb/port .
 #docker pull mongo
 docker run --name thisMongo -v ~/port/mongoVol:/data/db -it -d mongo:latest
 docker run --name port --link thisMongo:db -e VIRTUAL_HOST=port.lapela.dk -e PAGESPEED=1 -it -v ~/port/port:/www seeeb/port
